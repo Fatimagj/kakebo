d65c1abb14dc0d130dad276554313c88afe85842
Fatima Garcia Jorge <fatima.jorge.g@gmail.com>
2024-04-30 21:49:28 +0200

    tests: tipos de datos en fecha y cantidad

--- kakebo.py
+++ kakebo.py
@@ -1,8 +1,13 @@
+from datetime import date
 
 class Ingreso:
     def __init__(self, concepto, fecha, cantidad):
         if not isinstance(concepto, str):
             raise TypeError("Concepto debe ser cadena de texto")
+        if not isinstance(fecha, date):
+            raise TypeError("Fecha debe ser de tipo date")
+        if not (isinstance(cantidad, float) or isinstance(cantidad, int)):
+            raise TypeError("La cantidad tiene que ser numérica")
 
         self.concepto = concepto
         self.fecha = fecha
--- test_movimientos.py
+++ test_movimientos.py
@@ -10,13 +10,18 @@ def test_instanciar_ingreso():
     assert movimiento.cantidad == 1000
 
 def test_ingreso_concepto_debe_ser_string():
-   with pytest.raises(TypeError):
-    movimiento = Ingreso(19, date(2024, 1, 5), 1000) 
+    with pytest.raises(TypeError):
+        movimiento = Ingreso(19, date(2024, 1, 5), 1000) 
 
+def test_ingreso_fecha_typeError():
+    with pytest.raises(TypeError):
+        movimiento = Ingreso("Loteria del niño, premio", "1 de enero del 2024", 1000) 
 
-
-
-
+def test_ingreso_cantidad_typeError():
+    with pytest.raises(TypeError):
+        movimiento = Ingreso("Loteria del niño, premio", date(2024, 1, 5), "0") 
+    movimiento = Ingreso("Loteria del niño, premio", date(2024, 1, 5), 1000) 
+    movimiento = Ingreso("Loteria del niño, premio", date(2024, 1, 5), 1000.1) 
 
 
 
