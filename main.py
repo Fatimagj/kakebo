f34957eb1f4e44636feb9175001e1fce569b9d10
Fatima Garcia Jorge <fatima.jorge.g@gmail.com>
2024-05-05 19:50:39 +0200

    feat: añadimos funciones para instanciar, guardar e imprimir movimientos en una lista. Finalizadas las peticiones del 1 al 6 e imprimir la lista de ingresos y gastos

--- main.py
+++ main.py
@@ -18,8 +18,9 @@
 """
 from kakebo import Ingreso, Gasto, CategoriaGastos
 from datetime import date
-from enum import Enum
 
+
+listaMovimientos = []
 #validaciones
 def validarFecha(fecha):
     try:
@@ -27,82 +28,95 @@ def validarFecha(fecha):
     except ValueError:
         return False
 
+def imprimirMovimientos():
+    print("Movimientos registrados:")
+    for elemento in listaMovimientos:
+        print(elemento)
+    print("Fin de movimientos registrados")
 
-    
-#diferenciar entre ingresos y gastos
- 
-tipo = input("Ingreso, Gasto o Salir (I/G/S)").lower()
+def instanciarMovimiento(tipo, concepto, fecha, cantidad, categoria_elegida = ""):
+        fecha = date.fromisoformat(fecha)
+        if tipo == "i":
+            movimiento = Ingreso(concepto, fecha, cantidad)
+        if tipo == "g":
+            movimiento = Gasto(concepto, fecha, cantidad, categoria_elegida)
+        almacenarMovimiento(movimiento)
 
-while tipo not in ('i', 'g', 's'):
-    print("Teclea una de las funciones I, G o S")
+
+def almacenarMovimiento(movimiento):
+    listaMovimientos.append(movimiento)
+    print("Movimeinto registrado correctamente")
+
+continuar = True
+while continuar:
     tipo = input("Ingreso, Gasto o Salir (I/G/S)").lower()
-    
-    #ingresos
-if tipo == "i":
-    concepto = input("Concepto: ")
-    while len(concepto) < 5:
-        print("El concepto debe tener al menos 5 caracteres")
-        concepto = input("Concepto: ")
-    
-    fecha = input("Fecha: (YYYY-MM-DD): ")
-    while not validarFecha(fecha):
-        print("Introduzca una fecha valida (YYYY-MM-DD) y no futura")
-        fecha = input("Fecha (YYYY-MM-DD): ")
 
-    while True:
-        try: 
-            cantidad = float(input("Intruduce la cantidad en números: "))
-            break # sale del bucle si la conversión fue ok
-        except ValueError:
-            print("Error: por favor introduce la cantidad con números válidos y no letras ni símbolos.")
-
-    #gastos
-
-if tipo == "g":
-    concepto = input("Concepto: ")
-    while len(concepto) < 5:
-        print("El concepto debe tener al menos 5 caracteres")
-        concepto = input("Concepto: ")
-    
-    fecha = input("Fecha: (YYYY-MM-DD): ")
-    while not validarFecha(fecha):
-        print("Introduzca una fecha valida (YYYY-MM-DD) y no futura")
-        fecha = input("Fecha (YYYY-MM-DD): ")
-
-    while True:
-        try: 
-            cantidad = float(input("Intruduce la cantidad en números: "))
-            if cantidad < 0:
-                break
-            elif cantidad == 0:
-                cantidad = float(input("Introduce una cantidad que no sea 0"))
-            else:
-                cantidad *= -1
-                break 
-        except ValueError:
-            print("Por favor introduce la cantidad con números válidos y no letras ni símbolos.")
-    
-    print("Lista de categorías de gastos: ")
-    
-    for categoria in CategoriaGastos:
-        print(f"{categoria.value} - {categoria.name}")
-    while True:
-        eleccion_usuario = int(input("Introduce el número de la categoría de gastos: "))
-        if eleccion_usuario in [categoria.value for categoria in CategoriaGastos]:
-            categoria_elegida = CategoriaGastos(eleccion_usuario)
-            break
-        else:
-            print("El número introducido no corresponde a ninguna categoría válida.")
-
-#if tipo == "s":
-
-# 4.  Instanciar el movimiento (ingreso o gasto)
-
-def instanciarMovimiento(tipo):
+    while tipo not in ('i', 'g', 's'):
+        print("Teclea una de las funciones I, G o S")
+        tipo = input("Ingreso, Gasto o Salir (I/G/S)").lower()
+        
+        
     if tipo == "i":
-        ingreso = Ingreso(concepto, fecha, cantidad)
-    else:
-        tipo == "g"
-        gasto =  Gasto(concepto, fecha, cantidad, categoria_elegida)
+        concepto = input("Concepto: ")
+        while len(concepto) < 5:
+            print("El concepto debe tener al menos 5 caracteres")
+            concepto = input("Concepto: ")
+        
+        fecha = input("Fecha: (YYYY-MM-DD): ")
+        while not validarFecha(fecha):
+            print("Introduzca una fecha valida (YYYY-MM-DD) y no futura")
+            fecha = input("Fecha (YYYY-MM-DD): ")
 
-# 5.  
+        while True:
+            try: 
+                cantidad = float(input("Intruduce la cantidad en números: "))
+                break # sale del bucle si la conversión fue ok
+            except ValueError:
+                print("Error: por favor introduce la cantidad con números válidos y no letras ni símbolos.")
+        instanciarMovimiento(tipo, concepto, fecha, cantidad)
+
+    if tipo == "g":
+        concepto = input("Concepto: ")
+        while len(concepto) < 5:
+            print("El concepto debe tener al menos 5 caracteres")
+            concepto = input("Concepto: ")
+        
+        fecha = input("Fecha: (YYYY-MM-DD): ")
+        while not validarFecha(fecha):
+            print("Introduzca una fecha valida (YYYY-MM-DD) y no futura")
+            fecha = input("Fecha (YYYY-MM-DD): ")
+
+        while True:
+            try: 
+                cantidad = float(input("Intruduce la cantidad en números: "))
+                if cantidad < 0:
+                    break
+                elif cantidad == 0:
+                    cantidad = float(input("Introduce una cantidad que no sea 0"))
+                else:
+                    cantidad *= -1
+                    break 
+            except ValueError:
+                print("Error: Por favor introduce la cantidad con números válidos y no letras ni símbolos.")
+        
+        print("Lista de categorías de gastos: ")
+        
+        for categoria in CategoriaGastos: #imprime la lista de gastos
+            print(f"{categoria.value} - {categoria.name}")
+        while True:
+            eleccion_usuario = int(input("Introduce el número de la categoría de gastos: "))
+            if eleccion_usuario in [categoria.value for categoria in CategoriaGastos]:
+                categoria_elegida = CategoriaGastos(eleccion_usuario)
+                break
+            else:
+                print("El número introducido no corresponde a ninguna categoría válida.")
+        instanciarMovimiento(tipo, concepto, fecha, cantidad, categoria_elegida)
+
+    if tipo == "s":
+        imprimirMovimientos()
+        continuar = False
+
+
+    
+
+ 
